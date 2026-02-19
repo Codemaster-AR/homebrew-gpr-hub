4453470eeb364bcf2c15eefa26cbfa41545dabee
Anay Rustogi <codemaster.ar@gmail.com>
2026-02-19 14:42:24 +0400

    Update gpr-hub.rb
    Remove line numbers

--- Formula/gpr-hub.rb
+++ Formula/gpr-hub.rb
@@ -1,21 +1,20 @@
 class GprHub < Formula
-    2   include Language::Python::Virtualenv
-    3
-    4   desc "GPR Image Reader and AI Analyzer"
-    5   homepage "https://github.com/Codemaster-AR/gpr-hub-cli"
-    6   url "https://github.com/Codemaster-AR/gpr-hub-cli/archive/refs/tags/v4.0.0.tar.gz"
-    7   sha256 "84c7e6a87d987ed6b16ab4315e1704083a86ef06bb684e80d81d5879a032c44b"
-    8   license "MIT"
-    9
-   10   depends_on "python@3.12"
-   11   depends_on "sdl2"
-   12
-   13   def install
-   14     virtualenv_install_with_resources
-   15   end
-   16
-   17   test do
-   18     # Simple test to verify installation
-   19     system "#{bin}/gpr-hub", "--version"
-   20   end
-   21 end
+  include Language::Python::Virtualenv
+
+  desc "GPR Image Reader and AI Analyzer"
+  homepage "https://github.com/Codemaster-AR/gpr-hub-cli"
+  url "https://github.com/Codemaster-AR/gpr-hub-cli/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "84c7e6a87d987ed6b16ab4315e1704083a86ef06bb684e80d81d5879a032c44b"
+  license "MIT"
+
+  depends_on "python@3.12"
+  depends_on "sdl2"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/gpr-hub", "--version"
+  end
+end
