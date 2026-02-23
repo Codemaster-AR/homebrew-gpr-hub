50bfd93b931e369d5b157a00bbb3d2d3e8197eab
Anay Rustogi <codemaster.ar@gmail.com>
2026-02-23 21:59:47 +0400

    v5 cryptography bug fix

--- Formula/gpr-hub.rb
+++ Formula/gpr-hub.rb
@@ -9,6 +9,9 @@ class GprHub < Formula
 
   depends_on "python@3.12"
   depends_on "sdl2"
+  # Add explicit dependencies for `cryptography`
+  depends_on "libffi"
+  depends_on "openssl@3"
 
   def install
     # 1. Create the virtualenv
@@ -17,12 +20,20 @@ class GprHub < Formula
     # 2. Manually install pip inside the venv just in case it's missing
     system libexec/"bin/python", "-m", "ensurepip", "--upgrade"
 
+    # Set environment variables for compilation of native extensions like cryptography
+    # This ensures they link against Homebrew's openssl and libffi
+    ENV.prepend_path "PATH", venv/"bin"
+    ENV["LDFLAGS"] = "-L#{Formula["openssl@3"].opt_lib} -L#{Formula["libffi"].opt_lib}"
+    ENV["CFLAGS"] = "-I#{Formula["openssl@3"].opt_include} -I#{Formula["libffi"].opt_include}"
+    # Optional: This can sometimes help prevent conflicts by suppressing custom link flags from setup.py
+    # ENV["CRYPTOGRAPHY_SUPPRESS_LINK_FLAGS"] = "1"
+
     # 3. Use the venv's python to run pip and install your package
     # This ensures all dependencies in setup.py are handled
     system libexec/"bin/python", "-m", "pip", "install", "-v", "--ignore-installed", buildpath
 
     # 4. Link the executable
-    bin.install_symlink libexec/"bin/gpr-hub"
+    bin.install_symlink venv/"bin/gpr-hub"
   end
 
   test do
